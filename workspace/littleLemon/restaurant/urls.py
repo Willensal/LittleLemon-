a1af409bf82329f208adc97f9f24ff2826eb0542
Willensal <106775621+Willensal@users.noreply.github.com>
2024-05-21 14:54:16 -0400

    Update urls.py

--- workspace/littleLemon/restaurant/urls.py
+++ workspace/littleLemon/restaurant/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
 path('', views.home, name="home"),
 path('about/', views.about, name="about"),
 path('api-token-auth/', obtain_auth_token),
+path('book/', views.book, name="book"),
+path('menu/', views.menu, name="menu"),
 path('menu/', views.MenuItemView.as_view()),
 path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 path('',TemplateView.as_view(template_name='index.html'), name='indexpage'),
